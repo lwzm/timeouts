ed867173bab6b09e12f706b2f595a7cf9ce9548b
q <lwzm@qq.com>
2018-08-21 15:59:08 +0800

    tune start script

--- cython.sh
+++ cython.sh
@@ -3,4 +3,4 @@ gcc -shared -pthread -fPIC -fwrapv -O3 -Wall -fno-strict-aliasing \
     -I"$HOME/anaconda3/include/python3.6m" -o timeouts.so timeouts.c
 exit
 
-python -O -c 'from timeouts import server as s; s()'
+python -O -c 'import timeouts; timeouts.server()'
