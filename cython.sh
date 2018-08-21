1b1703877678d81d99b121965d110ff301334b91
q <lwzm@qq.com>
2018-08-21 10:49:04 +0800

    start script for server

--- cython.sh
+++ cython.sh
@@ -1,3 +1,6 @@
 cython -3 timeouts.py
 gcc -shared -pthread -fPIC -fwrapv -O3 -Wall -fno-strict-aliasing \
     -I"$HOME/anaconda3/include/python3.6m" -o timeouts.so timeouts.c
+exit
+
+python -O -c 'from timeouts import server as s; s()'
